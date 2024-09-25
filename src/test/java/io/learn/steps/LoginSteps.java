95a4a41c080c80484e6378b2b96957a341f28d40
Rakesh Vardan <rakeshlearnit@gmail.com>
2024-09-25 17:23:25 +0530

    Creates the step definitions

---
+++ src/test/java/io/learn/steps/LoginSteps.java
@@ -0,0 +1,23 @@
+package io.learn.steps;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class LoginSteps {
+
+    @Given("the user is on the login page")
+    public void the_user_is_on_the_login_page() {
+        System.out.println("User is on the login page");
+    }
+
+    @When("the user enters valid credentials")
+    public void the_user_enters_valid_credentials() {
+        System.out.println("User enters valid credentials");
+    }
+
+    @Then("the user should be redirected to the dashboard")
+    public void the_user_should_be_redirected_to_the_dashboard() {
+        System.out.println("User should be redirected to the dashboard");
+    }
+}
